e0283123dfa8d330c59153e900564bd2d309e87c
Tharan <tharan0026@gmail.com>
2024-08-14 22:55:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 3000); // Auto slide every 3 seconds
+
+// Initial display
+showSlide(currentSlide);
